447d0cf8af68b09e23b0e1eb4eeb537872617437
Sai Mane <saimanepatil1232@gmail.com>
2025-10-04 22:42:29 +0530

    Cleanup .gitignore and staging area

---
+++ app.js
@@ -0,0 +1,21 @@
+const express=require("express");
+const app=express();
+const mongoose=require("mongoose");
+
+const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+
+main().then(()=>{
+    console.log("connected to the db");
+}).catch((err)=>{
+    console.log(err);
+})
+
+app.listen(8080,()=>{
+    console.log("server is running at port 8080");
+})
+app.get("/",(req,res)=>{
+    res.send("working the root directory");
+})
