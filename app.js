13017d53b515498565c815f7ded37dc0aa8848c0
Sai Mane <saimanepatil1232@gmail.com>
2025-12-06 10:40:19 +0530

    implement error handling and added new error.ejs templet for it

--- app.js
+++ app.js
@@ -1,105 +1,102 @@
-const express=require("express");
-const app=express();
-const mongoose=require("mongoose");
-const Listing=require("./models/listing.js")
-const path=require("path");
-const methodOverride=require("method-override");
-const ejsMate=require("ejs-mate")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
 
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
-app.use(express.urlencoded({extended:true}));//parsing the data
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine('ejs', ejsMate);//used to create layouts
-app.use(express.static(path.join(__dirname,"/public")))
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
-const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
-async function main(){
-    await mongoose.connect(MONGO_URL);
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
 }
 
-main().then(()=>{
-    console.log("connected to the db");
-}).catch((err)=>{
-    console.log(err);
-})
+main()
+  .then(() => console.log("connected to the db"))
+  .catch((err) => console.log(err));
 
-app.listen(8080,()=>{
-    console.log("server is listening at port 8080");
-})
-app.get("/",(req,res)=>{
-    res.send("working the root directory");
+app.listen(8080, () => {
+  console.log("server is listening at port 8080");
 });
-//Index route
-app.get("/listings",async(req,res)=>{
-    const allListings =await Listing.find({})
-    res.render("./listings/index.ejs",{allListings})
 
-})
-//new route
-app.get("/listings/new",(req,res)=>{
-    res.render("./listings/new.ejs");
-})
-//show route(printing the data of individule listing)
-app.get("/listings/:id",async(req,res)=>{
-    let {id}=req.params;
-    const listing=await Listing.findById(id);
-    res.render("./listings/show.ejs",{listing});
-})
-//create route
-app.post("/listings",async(req,res)=>{
-   const newListing= new Listing(req.body.listing);
-   //req.body.listing gives just that inner object.
-   //creates a new document using your Mongoose model called Listing.
-   await newListing.save();
-    res.redirect("/listings")
-   console.log(listing);
-})
-//Edit route
-app.get("/listings/:id/edit",async(req,res)=>{
-    let {id}=req.params;
-    const listing=await Listing.findById(id);
-    res.render("./listings/edit.ejs",{listing});
+app.get("/", (req, res) => {
+  res.send("working the root directory");
+});
 
-})
-//Update route
-app.put("/listings/:id",async(req,res)=>{
-    let {id}=req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
-    res.redirect(`/listings/${id}`)
-})
-//Delete route
-app.delete("/listings/:id",async(req,res)=>{
-    let {id}=req.params;
-    let deletedListing= await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings")
+// Index route
+app.get("/listings", wrapAsync(async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("./listings/index.ejs", { allListings });
+}));
 
-})
+// New route
+app.get("/listings/new", (req, res) => {
+  res.render("./listings/new.ejs");
+});
+
+// Show route
+app.get("/listings/:id",wrapAsync(async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("./listings/show.ejs", { listing });
+}));
+
+// Create route
+app.post(
+  "/listings",
+  wrapAsync(async (req, res, next) => {
+    if (!req.body || !req.body.listing) {
+      throw new ExpressError(400, "Send valid data for listing");
+    }
+
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+  })
+);
 
 
+// Edit route
+app.get("/listings/:id/edit",wrapAsync(async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("./listings/edit.ejs", { listing });
+}));
+
+// Update route
+app.put("/listings/:id", wrapAsync(async (req, res) => {
+  const { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+}));
+
+// Delete route
+app.delete("/listings/:id",wrapAsync(async (req, res) => {
+  const { id } = req.params;
+  await Listing.findByIdAndDelete(id);
+  res.redirect("/listings");
+}));
+
+
+app.all(/.*/, (req, res, next) => {
+  next(new ExpressError(404, "Page not found!"));
+});
 
 
 
-
-
-
-
-
-
-
-
-// app.get("/testListing",async(req,res)=>{
-//     let sampleListing=new Listing({
-//         title:"My new Villa",
-//         discription:"By the beach",
-//         price:1200,
-//         location:"pune",
-//         country:"India"
-
-//     })
-//     await sampleListing.save();
-//     console.log("sample was saved")
-//     res.send("successfull");
-
-// })
+// Error Handler
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something went wrong" } = err;
+  // res.status(statusCode).send(message);
+  res.status(statusCode).render("./listings/error.ejs",{err});
+});
