e1e43f34682e1e35bb97e2a02621ac21c78c998d
Sai Mane <saimanepatil1232@gmail.com>
2025-12-21 17:10:28 +0530

    set up Cloudinary storage with multer and environment variables for image uploads

--- routes/listing.js
+++ routes/listing.js
@@ -7,16 +7,19 @@ const {isLoggedIn,isOwner,validateListing}=require("../middleware.js")
 const Listing = require("../models/listing.js")
 const listingController=require("../controller/listing.js")
 const multer  = require('multer')// handling multipart/form-data
-const upload = multer({ dest: 'uploads/' })
+const {storage}=require("../cloudConfig.js")
+const upload = multer({storage })//upload the image on the folder wanderlust_DEV in the cloudinary
 
 router
   .route("/")
   .get(wrapAsync(listingController.index))// Index route
-  // .post(isLoggedIn,validateListing,
-  // wrapAsync(listingController.createListing)// Create listing route
-  .post(upload.single('listing[image][url]'),(req,res)=>{
-    res.send(req.file);
-  });
+  .post(
+    isLoggedIn,
+    validateListing,
+    upload.single('listing[image][url]'),
+    wrapAsync(listingController.createListing)// Create listing route
+  );
+
 
 // New route
 router.get("/new",isLoggedIn,listingController.renderNewForm);
